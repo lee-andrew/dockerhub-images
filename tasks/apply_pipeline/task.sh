e74a6cba2e76ae028c23ed1edada9f6bf1d6840d
lee-andrew <lee.andrew076@gmail.com>
2017-12-23 04:23:46 -0500

    Removed downloading fly for the reapply pipeline

--- tasks/apply_pipeline/task.sh
+++ tasks/apply_pipeline/task.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-apt-get install wget
-wget https://github.com/concourse/concourse/releases/download/v3.8.0/fly_linux_amd64
-chmod +x fly*
-
-cp fly_linux_amd64 fly
-
-./fly -t ci login -c "http://$concourse_host:8080" --username=$concourse_username --password=$concourse_password
+fly -t ci login -c "http://$concourse_host:8080" --username=$concourse_username --password=$concourse_password
 
 pushd git_docker_image
-	../fly -t ci set-pipeline -n -p docker-image -c <(python pipeline.py) -l <(echo $creds)
+  fly -t ci set-pipeline -n -p docker-image -c <(python pipeline.py) -l <(echo $creds)
 popd
