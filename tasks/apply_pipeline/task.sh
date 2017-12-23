0aa855f8769bd1179ba957a4a497e2ef5b39be6b
lee-andrew <lee.andrew076@gmail.com>
2017-12-23 03:59:13 -0500

    Added tasks apply_pipeline

---
+++ tasks/apply_pipeline/task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+apt-get install wget
+wget https://github.com/concourse/concourse/releases/download/v3.8.0/fly_linux_amd64
+chmod +x fly*
+
+cp fly_linux_amd64 fly
+
+./fly -t ci login -c "http://$concourse_host:8080" --username=$concourse_username --password=$concourse_password
+
+pushd git_docker_image
+	../fly -t ci set-pipeline -n -p docker_image -c <(python pipeline.py) -l <(echo $creds)
+popd
